19b82c9cd9c0434bb33feaef7c25e808d7f58d7d
Justine <hell.justine@gmail.com>
2021-06-15 17:45:11 +0200

    Type object

--- app.ts
+++ app.ts
@@ -1,15 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return n1 + n2;
-  }
-}
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'Justine',
+  age: 30,
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);

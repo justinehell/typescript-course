eab3207b3817984399a7fc4e11690c7ac08ad925
Justine <hell.justine@gmail.com>
2021-06-16 17:09:00 +0200

    cleannup projet

---
+++ types/unkown-never-any.ts
@@ -0,0 +1,27 @@
+let userInput: any;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+userName = userInput;
+
+let userInput2: unknown;
+let userName2: string;
+
+userInput2 = 5;
+userInput2 = 'Max';
+//userName2 = userInput2; // error The 'unknown' is not assignable to type 'string'
+
+// we need to check the type before assigning the value of an unknown type to another variable with a fixed type
+if (typeof userInput === 'string') {
+  userName = userInput;
+}
+
+// unknown has at least some type checking, you cannot do whatever you want
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError('An error occured!', 500);
+console.log(generateError('An error occured!', 500)); // { nothing }

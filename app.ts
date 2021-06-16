53600c1104f26edfd7543e30a9234b286c6cfe1e
Justine <hell.justine@gmail.com>
2021-06-16 10:59:54 +0200

    Type aliases

--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
 function combine(
-  input1: number | string,
-  input2: number | string,
-  // resultConversion: string
-  resultConversion: 'as-number' | 'as-text'
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
 ) {
   let result;
   if (
@@ -14,11 +16,6 @@ function combine(
     result = input1.toString() + input2.toString();
   }
   return result;
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
 }
 
 const combinedAges = combine(30, 26, 'as-number');

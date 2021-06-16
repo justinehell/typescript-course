f5f186eac322ed6a421353a559b904ed7c7c488a
Justine <hell.justine@gmail.com>
2021-06-16 11:37:22 +0200

    Type unknown (VS any)

--- app.ts
+++ app.ts
@@ -1,30 +1,20 @@
-function add(n1: number, n2: number): number {
-  return n1 + n2;
+let userInput: any;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+userName = userInput;
+
+let userInput2: unknown;
+let userName2: string;
+
+userInput2 = 5;
+userInput2 = 'Max';
+userName2 = userInput2; // error The 'unknown' is not assignable to type 'string'
+
+// we need to check the type before assigning the value of an unknown type to another variable with a fixed type
+if (typeof userInput === 'string') {
+  userName = userInput;
 }
 
-function printResult(num: number): void {
-  console.log('Result: ', +num);
-}
-
-printResult(add(5, 12)); // Result: 17
-
-let combinedValues: Function;
-combinedValues = add;
-console.log(combinedValues(8, 8)); // 16
-
-//combinedValues = 5; // error
-
-combinedValues = printResult;
-console.log(combinedValues(8, 8)); // undefined because printResult only have 1 param
-
-let combinedValuesWithoutParams: () => number; // define a function without params, return a number
-let combinedValuesWithParams: (a: number, b: number) => number; // define a function with 2 params number, return a number
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
+// unknown has at least some type checking, you cannot do whatever you want
